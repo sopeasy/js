74afe60b2e056ae2396b63a219e7cdba947e669f
d3ciee <decefemz@gmail.com>
2025-01-28 16:23:43 +0200

    feat: add setProfile method and cleanup script

--- peasy.js
+++ peasy.js
@@ -1,65 +1,60 @@
 ((window) => {
     if (!window.document.currentScript) return;
 
-    const INGEST_EVENT_PATH = "e";
+    const POST_EVENT_PATH = 'e';
+    const POST_PROFILE_PATH = 'p';
+    const VISITOR_ID_LOCALSTORAGE_KEY = 'peasy-visitor-id';
 
-    config = {
+    const config = {
         websiteId: '',
         ingestUrl: 'https://api.peasy.so/v1/ingest/',
         maskPatterns: [],
         autoPageView: true,
         ignoreQueryParams: false
-    }
+    };
 
-    const attr = window.document.currentScript.getAttribute.bind(document.currentScript);
+    const attr = window.document.currentScript.getAttribute.bind(window.document.currentScript);
+
+    config.ingestUrl = attr('data-ingest-url') || config.ingestUrl;
+    if (!config.ingestUrl.endsWith('/')) {
+        config.ingestUrl += '/';
+    }
 
     config.websiteId = attr('data-website-id');
-    if (!config.websiteId) {
-        console.error(`[peasy.js] Error: website-id is required`);
-        return;
-    }
-
-    const ingestUrl = attr('data-ingest-url');
-    if (ingestUrl) {
-        try {
-            new URL(ingestUrl);
-            config.ingestUrl = ingestUrl;
-        } catch (e) {
-            console.error(`[peasy.js] Error: ingest-url is invalid`);
-            console.warn(`[peasy.js] Warn: ingest-url is invalid, using default`);
-        }
-    }
-
-    const maskPatterns = JSON.parse(attr('data-mask-patterns'));
-    if (maskPatterns) {
-        try {
-            const maskPatternsArr = JSON.parse(maskPatterns);
-            if (!Array.isArray(maskPatternsArr)) {
-                throw new Error("Invalid mask patterns");
-            }
-            config.maskPatterns = maskPatterns;
-        } catch (e) {
-            console.warn(`[peasy.js] Warn: mask-patterns are invalid, ignoring`);
-        }
-    }
-
+    config.maskPatterns = JSON.parse(attr('data-mask-patterns') || '[]');
     config.autoPageView = attr('data-auto-page-view') !== "false";
     config.ignoreQueryParams = attr('data-ignore-query-params') === "true";
 
-    const send = (path, payload) => {
-        const url = new URL(path, config.ingestUrl).href;
-        try {
-            if (!navigator?.sendBeacon(url, JSON.stringify(payload))) {
-                fetch(url, {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(payload),
-                    keepalive: true,
-                });
+    if (!config.websiteId) {
+        console.warn('[peasy.js] Website ID not provided');
+        return;
+    }
+
+    let lastPage = null;
+
+    const maskPathname = (maskPattern, pathname) => {
+        const normalizePath = (path) =>
+            path.endsWith('/') ? path.slice(0, -1).split('/') : path.split('/');
+
+        const maskSegments = normalizePath(maskPattern);
+        const pathSegments = normalizePath(pathname);
+
+        if (pathSegments.length > maskSegments.length) return pathname;
+
+        const maskedSegments = [];
+        for (let i = 0; i < maskSegments.length; i++) {
+            const maskSegment = maskSegments[i];
+            const pathSegment = pathSegments[i];
+
+            if (maskSegment === '*') {
+                maskedSegments.push('*');
+            } else if (pathSegment && maskSegment === pathSegment) {
+                maskedSegments.push(pathSegment);
+            } else {
+                return pathname;
             }
-        } catch (e) {
-            console.error(`[peasy.js] Error: ${e.message}`);
         }
+        return maskedSegments.join('/');
     };
 
     const processUrl = (url) => {
@@ -73,117 +68,90 @@
                 }
             }
         }
-        if (config.ignoreQueryParams) {
-            _url.search = "";
-        }
+        if (config.ignoreQueryParams) _url.search = '';
         return _url.href;
     };
 
     const getReferrer = () => {
-        return !window.document.referrer.includes(location.hostname) ? window.document.referrer : "";
-    }
-
-    const maskPathname = (maskPattern, pathname) => {
-        const maskSegments = (() => {
-            if (maskPattern.endsWith('/')) {
-                return url.slice(0, -1);
-            }
-            return maskPattern;
-        })().split('/');
-
-        const pathSegments = (() => {
-            if (pathname.endsWith('/')) {
-                return url.slice(0, -1);
-            }
-            return pathname;
-        })().split('/');
-
-
-        if (pathSegments.length > maskSegments.length) {
-            return pathname;
-        }
-
-        const maskedSegments = [];
-
-        for (let i = 0; i < maskSegments.length; i++) {
-            const maskSegment = maskSegments[i];
-
-            if (maskSegment === '*') {
-                maskedSegments.push('*');
-            } else {
-                if (pathSegments[i] !== undefined) {
-                    maskedSegments.push(pathSegments[i]);
-                } else {
-                    maskedSegments.push('');
-                }
-            }
-        }
-
-        const maskedPath = maskedSegments.join('/');
-        return maskedPath;
+        return !document.referrer.includes(location.hostname) ? document.referrer : '';
     };
 
-    const registerPushListener = () => {
-        const hook = (_this, method, callback) => {
-            const orig = _this[method];
+    const send = (path, payload) => {
+        try {
+            const url = new URL(path, config.ingestUrl).href;
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Visitor-ID': localStorage.getItem(VISITOR_ID_LOCALSTORAGE_KEY) || '',
+                },
+                body: JSON.stringify(payload),
+                keepalive: true,
+            }).then(r => {
+                const visitorId = r.headers.get('X-Visitor-ID');
+                if (visitorId) {
+                    localStorage.setItem(VISITOR_ID_LOCALSTORAGE_KEY, visitorId);
+                }
+            });
+        } catch (e) {
+            console.error('[peasy.js] Error:', e);
+        }
+    };
+
+    const track = (name, metadata) => {
+        const payload = {
+            name,
+            website_id: config.websiteId,
+            page_url: processUrl(location.href),
+            host_name: location.hostname,
+            referrer: getReferrer(),
+            lang: navigator.language,
+            screen: `${screen.width}x${screen.height}`,
+            metadata: metadata || {},
+        };
+        send(POST_EVENT_PATH, payload);
+    };
+
+    const setProfile = (profileId, profile) => {
+        send(POST_PROFILE_PATH, {
+            website_id: config.websiteId,
+            host_name: location.hostname,
+            profile_id: profileId,
+            profile: profile,
+        });
+    };
+
+    const page = () => {
+        if (lastPage === location.pathname) return;
+        lastPage = location.pathname;
+        track('$page_view', { page_title: document.title });
+    };
+
+    if (config.autoPageView) {
+        page();
+
+        const hook = (history, method, callback) => {
+            const orig = history[method];
             return (...args) => {
-                callback.apply(null, args);
-                return orig.apply(_this, args);
+                callback(...args);
+                return orig.apply(history, args);
             };
         };
-        const handlePush = (_, title, url) => {
+
+        const handlePush = (state, title, url) => {
             if (!url) return;
-
-            const urlBeforePush = window.location.href;
-            const urlAfterPush = url.toString();
-
-            if (urlBeforePush !== urlAfterPush) {
-                const payload = {
-                    website_id: config.websiteId,
-                    name: "$page_view",
-                    referrer: getReferrer(),
-                    page_url: processUrl(urlAfterPush),
-                    host_name: window.location.hostname,
-                    lang: window.navigator.language,
-                    screen: `${screen.width}x${screen.height}`,
-                    metadata: { title }
-                }
-                const t = setTimeout(() => {
-                    send(INGEST_EVENT_PATH, payload);
-                    clearTimeout(t)
-                }, 100)
-            }
+            const currentUrl = location.href;
+            const newUrl = url.toString();
+            if (currentUrl !== newUrl) setTimeout(page, 100);
         };
 
-        window.history.pushState = hook(window.history, 'pushState', handlePush);
-        window.history.replaceState = hook(window.history, 'replaceState', handlePush);
-    };
-
-    const track = async (name, metadata) => {
-        const payload = {
-            website_id: config.websiteId,
-            name,
-            referrer: getReferrer(),
-            page_url: processUrl(window.location.href),
-            host_name: window.location.hostname,
-            lang: window.navigator.language,
-            screen: `${screen.width}x${screen.height}`,
-            metadata,
-        }
-        send(INGEST_EVENT_PATH, payload);
+        history.pushState = hook(history, 'pushState', handlePush);
+        history.replaceState = hook(history, 'replaceState', handlePush);
     }
-
-    const page = () => track("$page_view");
-
-    page();
-    registerPushListener();
 
     window.peasy = {
         track,
-        page
-    }
-})(window)
-
-
-
-
+        page,
+        setProfile,
+    };
+})(window);
